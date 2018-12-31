aabfa391a1ddd9d47f14abdd9f6f75f94a28e419
Murali Krishna <murali@highgear.io>
2018-12-31 11:02:06 +0530

    Deleted junk

--- application/models/mr_job_status.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint,jsonify
-from application.models.models import TblCustomerJobRequest,TblMetaMrRequestStatus
-from sqlalchemy.orm import scoped_session
-from application import session_factory
-
-mrjobstatus=Blueprint('mrjobstatus',__name__)
-@mrjobstatus.route("/clusterlocation/cloudtype",methods=['GET'])
-def mrJobStatus():
-    session = scoped_session(session_factory)
--- application/modules/apis/metaclusterlist.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask,jsonify,request,Blueprint
-from application.models.models import TblUsers
-from sqlalchemy.orm import scoped_session
-from application import session_factory
-
-metaclusterist=Blueprint('',__name__)
-
-@metaclusterist.route("/users/<customer_id>", methods=['GET'])
