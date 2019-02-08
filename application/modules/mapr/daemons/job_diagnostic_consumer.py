691d670547f463b8d045c9b5736ea062755c01ba
shirishkrishna <shirishkrishnasai@gmail.com>
2019-02-08 14:29:52 +0530

    modified job diagnostics api

--- application/modules/mapr/daemons/job_diagnostic_consumer.py
+++ application/modules/mapr/daemons/job_diagnostic_consumer.py
@@ -12,34 +12,30 @@ jobdiagnostics = Blueprint('jobdiagnostics', __name__)
 @jobdiagnostics.route("/api/jobdiagnostics", methods=['POST'])
 
 def diagnosticsconsumer():
-    #try:
+    try:
         db_session = scoped_session(session_factory)
-        print "in job diagnostics consumerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr"
+        #print "in job diagnostics consumerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr"
         data = request.json
-        print data,type(data),'dataa postedddddd'
+        #print data,type(data),'dataa postedddddd'
         state = str(data['app']['state'])
-        print state,type(state),'sattttttttttttt'
+        #print state,type(state),'sattttttttttttt'
         request_status_value = db_session.query(TblMetaMrRequestStatus.srl_id).\
             filter(TblMetaMrRequestStatus.var_mr_request_status == state).all()
-        print request_status_value[0][0],'valueeeeeeeeeeeeeeeeeeeeeeee'
-        print data['app'],'apppppppp'
-        #for message in data.items():
-        #    print message
-        #    json_loads_job_data = json.loads(message)
-        #    print json_loads_job_data,'jsonnnnnnnnnnnn'
+        #print request_status_value[0][0],'valueeeeeeeeeeeeeeeeeeeeeeee'
+        #print data['app'],'apppppppp'
         print data['customer_id'],data['request_id']
         update_customer_job_request=db_session.query(TblCustomerJobRequest).\
             filter(TblCustomerJobRequest.uid_customer_id==data['customer_id'],TblCustomerJobRequest.uid_request_id==data['request_id'])
         print "after queryyyy"
         update_customer_job_request.update({"var_job_diagnostics":json.dumps(data['app']),"int_request_status":int(request_status_value[0][0])})
         db_session.commit()
-    # except Exception as e:
-    #     exc_type, exc_obj, exc_tb = sys.exc_info()
-    #     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-    #
-    #     my_logger.error(exc_type)
-    #     my_logger.error(fname)
-    #     my_logger.error(exc_tb.tb_lineno)
-    # finally:
-    #     db_session.close()
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+
+        my_logger.error(exc_type)
+        my_logger.error(fname)
+        my_logger.error(exc_tb.tb_lineno)
+    finally:
+        db_session.close()
 
